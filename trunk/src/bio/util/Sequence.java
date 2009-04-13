ec04ed918c584f67a1ddff56a2dc25411a136d98
anoopkum <anoopkum@80b5d37c-27d6-11de-9600-dfb65e630a92>
2009-04-13 03:36:58 +0000

    added sequence label

---
+++ trunk/src/bio/util/Sequence.java
@@ -0,0 +1,36 @@
+/*
+ * Sequence.java
+ *
+ * Created on February 17, 2008, 3:37 PM
+ *
+ * <p><b>License and Copyright: </b>The contents of this file are subject to the
+ * Mozilla Public License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License
+ * at <a href="http://www.mozilla.org/MPL">http://www.mozilla.org/MPL/.</a></p>
+ *
+ * <p>Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.</p>
+ *
+ * <p>The entire file consists of original code.  Copyright &copy; 2003-2007
+ * Tufts University. All rights reserved.</p>
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+package bio.util;
+
+/**
+ *  A simple data structure to save the label and and sequeunce of protein
+ * sequnece
+ * @author akumar03
+ *
+ */
+public class Sequence {
+    public String label = new String();
+    public String sequence = new String();
+    /** Creates a new instance of Sequence */
+    public Sequence() {
+    }
+    
+}
