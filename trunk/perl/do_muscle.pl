7882884d77ba3aa409cdc5cacf90060a3cd77741
anoopkum <anoopkum@80b5d37c-27d6-11de-9600-dfb65e630a92>
2009-04-23 01:58:11 +0000

    added hmmbuild file

--- trunk/perl/do_muscle.pl
+++ trunk/perl/do_muscle.pl
@@ -5,14 +5,14 @@
 
 $prog_muscle = "/r/bcb/anoop/soft/muscle3.7/muscle ";
 
-$exp_file = "Scop95.csv";
+$exp_file = "astralStats.txt";
 print "reading exp file $exp_file";
 
 open(INPUT,$exp_file) or die("Can't open the input file: $exp_file");
 while($line = <INPUT>){
  print "Reading line $line";
- @words = split(',',$line);
- for($j=0;$j<=25;$j+=5) {
+ @words = split('\t',$line);
+ for($j=0;$j<=0;$j+=5) {
   $exp_class = $words[0]."_".$j;
   $com1 = $prog_muscle." -out ".$exp_class.".aln -in ".$exp_class.".fasta";
    print "Executing:  $com1\n";
