6687b3dc854600ffdecfe41b736ce2dfbbf97f2f
anoopkum <anoopkum@80b5d37c-27d6-11de-9600-dfb65e630a92>
2009-06-15 17:24:01 +0000

    run on mutated set from 0-60

--- trunk/perl/do_hmmbuild_all.pl
+++ trunk/perl/do_hmmbuild_all.pl
@@ -3,7 +3,7 @@
 # Date : 04/22/09
 # Description : A program that  builds hmms for 0, 20%, NE and NEX mutation alignment files
 
-$prog_hmm = "/r/bcb/anoop/soft/hmmer-2.4i/src/hmmbuild --effent -eloss 0.7 ";
+$prog_hmm = "/r/bcb/anoop/soft/hmmer-2.4i/src/hmmbuild --effent --eloss 0.7 ";
 
 
 $exp_file = "astralStats.txt";
@@ -14,17 +14,13 @@ while($line = <INPUT>){
  print "Reading line $line";
  @words = split('\t',$line);
  $exp_class = $words[0];
-  $com1 =  $prog_hmm.$exp_class."_0.hmm ".$exp_class."_0.aln";
+  for($i=10;$i<=60;$i += 10) {
+  print `$com1`;
+  $com1 =  $prog_hmm.$exp_class."_".$i.".hmm ".$exp_class."_".$i.".aln";
   print "Executing:  $com1\n";
   print `$com1`;
-  $com1 =  $prog_hmm.$exp_class."_20.hmm ".$exp_class."_20.aln";
+  $com1 =  $prog_hmm.$exp_class."_".$i."NE.hmm ".$exp_class."_".$i."NE.aln";
   print "Executing:  $com1\n";
   print `$com1`;
-  $com1 =  $prog_hmm.$exp_class."_20NE.hmm ".$exp_class."_20NE.aln";
-  print "Executing:  $com1\n";
-  print `$com1`;
-  $com1 =  $prog_hmm.$exp_class."_20NEX.hmm ".$exp_class."_20NEX.aln";
-  print "Executing:  $com1\n";
-  print `$com1`;
+ }
 }
-
