799d30b27e9544f1cb2d79dc36f7827140f6d1ff
anoopkum <anoopkum@80b5d37c-27d6-11de-9600-dfb65e630a92>
2009-12-28 19:26:38 +0000

    align and then mutate

---
+++ perl/run_mutation_exp2.pl
@@ -0,0 +1,79 @@
+# Program: run_mutation_exp.pl
+# Developed by : Anoop Kumar
+# Date: 12/24/2009
+# Description: This program run creates an HMM with blosum mutations 40 times and assess the stability. This first does alignment and then generates mutations
+
+
+use strict;
+use Time::HiRes qw(usleep);
+
+my $exp_file = $ARGV[0];
+my $N = $ARGV[1];
+my $MUTATION = 20;
+my $PATH = "/cluster/tufts/protein/se2/athmm/perl/";
+print "reading exp file $exp_file N=$N\n";
+
+
+open(OUT,">ext_mut".$MUTATION."_a".$N.".txt");
+
+open(INPUT,$exp_file) or die("Can't open the input file: $exp_file");
+
+print OUT " \t";
+while(my $exp_line = <INPUT>){
+ my @exp_words = split(',',$exp_line);
+ print OUT $exp_words[0]."\t";
+}
+close INPUT;
+print OUT "\n";
+
+open(INPUT,$exp_file) or die("Can't open the input file: $exp_file");
+while(my $exp_line = <INPUT>){
+  my @exp_words = split(',',$exp_line);
+  my $com = "/cluster/tufts/protein/muscle/mus4 -i ".$exp_words[0]."_0.fasta -o ".$exp_words[0]."_0.aln";
+  print "$com\n";
+  print `$com`;
+  
+}
+close INPUT;
+
+for(my $i=0;$i<40;$i++) {
+   print OUT "$i\t";
+  open(INPUT,$exp_file) or die("Can't open the input file: $exp_file");
+  while(my $exp_line = <INPUT>){
+    my @exp_words = split(',',$exp_line);
+    my $exp_class =  $exp_words[0];
+    my $exp_class_file =  $exp_class."_".$MUTATION."_a".$N;
+    my $com ="perl ".$PATH."generate_mutations2.pl $exp_class $N $i";
+    print "$com\n";
+    print `$com`;
+    $com = "perl ".$PATH."aln2ssi.pl $exp_class_file";
+    print "$com\n";
+    print `$com`;
+    $com = "/cluster/tufts/protein/se2/hmmer/hmmer-3.0a2/src/hmmbuild ".$exp_class_file.".hmm $exp_class_file.ssi";
+    print "$com\n";
+    print `$com`;
+    $com = "/cluster/tufts/protein/se2/hmmer/hmmer-3.0a2/src/hmmsearch -E 1000 --max ".$exp_class_file.".hmm ".$exp_class."_test.fasta > ".$exp_class_file.".hit";
+    print "$com\n";
+    print `$com`;
+    $com = "perl /cluster/tufts/protein/se2/athmm/perl/mep.pl ".$exp_class_file.".hit";
+    print "$com\n";
+    print `$com`;
+    my $mep = read_mep();
+    print OUT "$mep\t"; 
+    usleep(100);
+    print "$exp_class MEP $mep\n"; 
+  }
+    print OUT "\n"; 
+
+}
+
+close OUT;
+
+sub read_mep {
+   open(MEP,"mep.txt");
+   my $line;
+   if($line = <MEP>) {
+   }
+   close MEP;
+   return $line;
+}
