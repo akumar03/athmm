4d7bb9a02449815d1bc2d1f052913131577729c0
anoopkum <anoopkum@80b5d37c-27d6-11de-9600-dfb65e630a92>
2010-01-01 20:12:18 +0000

    program working for single and beta mutations

--- trunk/perl/run_all.pl
+++ trunk/perl/run_all.pl
@@ -20,7 +20,7 @@ my $MAX_RATE = 60;
 my $MIN_SRATE =5;
 my $MAX_SRATE =25;
 
-
+my $EMPTY = "                               ";
 my $PROTEIN_LETTERS = "ACDEFGHIKLMNPQRSTVWY";
 my $exposed_file = "ExposedProbability.csv";
 my $burried_file = "BurriedProbability.csv";
@@ -32,27 +32,32 @@ my @sequences;
 my @new_sequences;
 my $total_beta;
 
+my $BLOSUM_FILE = "/cluster/tufts/protein/se2/athmm/SortedBlosum62.csv";
+my $AA_LETTERS = "ACDEFGHIKLMNPQRSTVWY";
+my @AAF = (0.074,0.025,0.054,0.054,0.047,0.074,0.026,0.068,0.099,0.058,0.025,0.045,0.039,0.034,0.052, 0.057,0.051,0.073,0.013,0.034);
+my $L = 0.347;
 
-print "reading exp file $exp_file N=$N\n";
+my @probs =load_blosum();
+
+
 open(OUT,">ext_mut_all.txt");
 print OUT " \t0\t";
-for(my $mrate = $MIN_RATE;$mrate<=$MAX_RATE;$mrate +=10) {
-  print OUT "$mrate\t";
-#  my $com = "perl /cluster/tufts/protein/se2/athmm/perl/generate_betamutations.pl $exp_file 0 $mrate $N";
-#   print "$com\n";
-#   print `$com`;
+for(my $mrate = 0;$mrate<=$MAX_RATE;$mrate +=10) {
+    for(my $srate=0;$srate<=$MAX_SRATE;$srate +=5) {
+      print OUT $mrate."_".$srate."\t";
+   }
 }
 print OUT "\n";
   open(INPUT,$exp_file) or die("Can't open the input file: $exp_file");
-  if(my $exp_line = <INPUT>){
+  while(my $exp_line = <INPUT>){
    my @exp_words = split(',',$exp_line);
    my $exp_class = $exp_words[0];
    print  OUT "$exp_class\t"; 
-   my $com = "perl ".$PATH."aln2ssi.pl $exp_class_file";
+   my $com = "/cluster/tufts/protein/se2/hmmer/hmmer-3.0a2/src/hmmbuild ".$exp_class."_0_0.hmm ".$exp_class."_0_matt.ssi";
    print "$com\n";
    print `$com`;
     
-   my $com = "/cluster/tufts/protein/se2/hmmer/hmmer-3.0a2/src/hmmsearch -E 1000 --max ".$exp_class."_0_0.hmm ".$exp_class."_test.fasta > ".$exp_class."_0.hit";
+  $com = "/cluster/tufts/protein/se2/hmmer/hmmer-3.0a2/src/hmmsearch -E 1000 --max ".$exp_class."_0_0.hmm ".$exp_class."_test.fasta > ".$exp_class."_0_0.hit";
    print "$com\n";
    print `$com`;
    $com = "perl /cluster/tufts/protein/se2/athmm/perl/mep.pl ".$exp_class."_0_0.hit";
@@ -60,14 +65,51 @@ print OUT "\n";
    print `$com`;
    my $mep = read_mep();
    print OUT "$mep\t";
-   
-   for(my $mrate = $MIN_RATE;$mrate<=$MAX_RATE;$mrate +=10) {
-    $exp_class =  $exp_words[0]."_0_matt_".$mrate;
-    my $ssi_file = $exp_words[0]."_0_matt_".$mrate.".ssi";
+   $total_beta = 0; 
+   my $ssi_file = $exp_words[0]."_0_matt.ssi";
+    open(SSI,$ssi_file);
+    while(my $line = <SSI>) {
+      if($line =~ m/^#=/) {
+        my @words = split(/\s+/,$line);
+        my $beta = new Beta($words[1],$words[2],$words[3],$words[5],$words[6]);
+        $total_beta += $words[3];  # assuming no overlap
+        push(@betas,$beta);
+      } 
+      if($line =~ m/^d/) {
+         push (@sequences,$line);
+      }
+     }
+     $total_beta *=2;
+     print "Total beta = $total_beta\n";
+    close (SSI);
+   for(my $mrate = 0;$mrate<=$MAX_RATE;$mrate +=10) {
+    for(my $srate=0;$srate<=$MAX_SRATE;$srate +=5) {
+      @new_sequences = ();
 
-    
-    $com = "/cluster/tufts/protein/se2/hmmer/hmmer-3.0a2/src/hmmbuild ".$exp_class.".hmm $ssi_file";
-    print "$com\n";
+#adding beta mutated sequences
+    for(my $i=0;$i<=$#sequences;$i++) {
+       push(@new_sequences,$sequences[$i]);
+       if($mrate>0) {
+        for(my $j =0;$j<$N;$j++) {
+          my $new_sequence = get_beta_mutated_sequence($sequences[$i],$mrate,$j);
+          push(@new_sequences,$new_sequence);
+        }
+       }
+       if($srate>0) {
+
+        for(my $j =0;$j<$N;$j++) {
+ 	  push(@new_sequences,get_mutated_sequence($sequences[$i],$j,$srate));
+         }
+      }
+    }    
+       
+    $exp_class =  $exp_words[0]."_".$mrate."_".$srate;
+    my $out_file = $exp_class.".ssi";
+    print "Writing $out_file\n";
+    write_out_file($out_file);
+
+    $com = "/cluster/tufts/protein/se2/hmmer/hmmer-3.0a2/src/hmmbuild ".$exp_class.".hmm ".$exp_class.".ssi";
+        print "$com\n";
     print `$com`;
     $com = "/cluster/tufts/protein/se2/hmmer/hmmer-3.0a2/src/hmmsearch -E 1000 --max ".$exp_class.".hmm ".$exp_words[0]."_test.fasta > ".$exp_class.".hit";
     print "$com\n";
@@ -75,14 +117,14 @@ print OUT "\n";
     $com = "perl /cluster/tufts/protein/se2/athmm/perl/mep.pl ".$exp_class.".hit";
     print "$com\n";
     print `$com`;
-    my $mep = read_mep();
-    print OUT "$mep\t"; 
     usleep(100);
+    my $mep = read_mep();
+    print OUT $mep."\t"; 
     print "$exp_class MEP $mep\n"; 
+    }
    }
     print OUT "\n"; 
   }
-
 close OUT;
 
 sub read_mep {
@@ -186,8 +228,9 @@ sub get_beta_mutated_sequence{
  $seq_letters = $seq_mutated;
 # print "$position $mutate_position $mutated_position $is_burried\n";
  }
- my $new_sequence = $seq_label."_m".$count."         ".$seq_letters;
-
+ my $new_label = $seq_label."_k".$count;
+ my $new_sequence = $new_label.substr($EMPTY,0,21-length($new_label)).$seq_letters;
+# print "label: ".$new_label." len:".length($new_label)."\n";
 # print "$position $mutate_position $mutated_position $is_burried\n";
 # print "$position $mutate_position $mutated_position $is_burried\n";
 # print $sequence;
@@ -220,12 +263,102 @@ sub get_mutated_aa {
 
 sub write_out_file {
   my $out_file = shift;
-  open(OUT,">$out_file");
-  print OUT "# STOCKHOLM 1.0\n";
+  open(OUTS,">$out_file");
+  print OUTS "# STOCKHOLM 1.0\n";
   for(my $i=0;$i<= $#new_sequences;$i++) {
-     print OUT $new_sequences[$i];
+     print OUTS $new_sequences[$i];
   }
-  print OUT "//\n";
-  close OUT;
+  print OUTS "//\n";
+  close OUTS;
+}
+
+sub load_blosum {
+  my @probs;
+  open(BLOSUM,$BLOSUM_FILE) or die("Can't open blosum file");
+  my $count = 0;
+  while(my $line = <BLOSUM>) {
+    my @words = split(",",$line);
+    my $total = 0;
+    for(my $i =0;$i<=$#words;$i++) {
+       my $freq = int($words[$i]);
+       $probs[$count][$i] = (10**($L*$freq))*$AAF[$count]*$AAF[$i];
+#       print $probs[$count][$i]." ".$words[$i]."\n";
+        $total += $probs[$count][$i];
+    }
+   
+   for(my $i =0;$i<=$#words;$i++) {
+          $probs[$count][$i] =$probs[$count][$i]/$total;
+#          print "$count $i ". $probs[$count][$i]."\n";
+   }
+    $count++;
+  }
+  close(BLOSUM);
+  return @probs;
+}
+
+sub get_random {
+  my $char = shift;
+  my $random = rand();
+   $char = uc $char;
+  my $aa_index = index($AA_LETTERS,$char);
+  my $mutate_index = 0;
+  for(my $i=0;$i<20 && $random >0;$i++) {
+     $random -= $probs[$aa_index][$i];
+#     print "$char $aa_index $random $probs[$aa_index][$i] $i \n";
+     $mutate_index = $i;
+   }
+   return substr($AA_LETTERS,$mutate_index,1);
+}
+
+sub read_fasta_file {
+   my $file = shift;
+   my @seqs;
+   my $label;
+   my $sequence;
+   print "Reading file $file\n";
+   open(FILE,$file) or die("Can't open $file");
+   my $count = 0;
+   while(my $line = <FILE>) {
+#     chop($line);
+#     print $line;
+     if($line =~ m/\>/) {
+          if($count > 0) {
+                $sequence =~ s/\W//g;
+                $sequence =~ s/\r\n//g;
+                $sequence =~ s/\n//g;
+                $label =~ s/\r$//;
+                push(@seqs,$label."\t".$sequence);
+#               print "count $count\n  $label $sequence \n";
+          }
+          $label = $line;
+          $sequence ="";
+          $count++;
+     } else {
+           $sequence .= $line;
+    }
+  }  
+  push(@seqs,$label."\t".$sequence);
+  return @seqs;
+}
+
+sub get_mutated_sequence {
+  my $s = shift;
+  my $index = shift;
+  my $mutations = shift;
+  my @s_parts = split(/\s+/,$s);
+  my $seq_label = $s_parts[0];
+  my $sequence = $s_parts[1];
+  my $n_mutations = int($mutations*length($sequence)/100+0.5);
+  for(my $i = 0;$i<$n_mutations;$i++) {
+    my $mutation_index = int(rand()*length($sequence));
+    while(substr($sequence,$mutation_index,1) ne "-") {
+      $mutation_index = int(rand()*length($sequence));
+    }
+    $sequence = substr($sequence,0,$mutation_index).get_random(substr($sequence,$mutation_index,1)).substr($sequence,$mutation_index+1);
+  }
+ my $new_label = $seq_label."_a".$index;
+#  print "$seq_label $index size:".length($new_label)."\n";
+ my $new_s = $new_label.substr($EMPTY,0,21-length($new_label)).$sequence;
+  return $new_s."\n";
 }
 
